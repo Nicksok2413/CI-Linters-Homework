45a29d4c2633378fe51f6ad832aa7d4812240a31
nick <nicksok2413@gmail.com>
2025-03-22 12:16:29 +0300

    fix main.py

--- src/main.py
+++ src/main.py
@@ -29,18 +29,20 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
         yield session
 
 
+get_db_dep = Depends(get_db)
+
+
 @app.get(
     "/recipes",
     response_model=list[RecipesOut],
     summary="Get all recipes",
     description="Returns a list of recipes sorted by views & cooking time.",
 )
-async def get_all_recipes(db: AsyncSession = Depends(get_db)) -> Sequence[Recipe]:
+async def get_all_recipes(db: AsyncSession = get_db_dep) -> Sequence[Recipe]:
     result = await db.execute(
         select(Recipe).order_by(Recipe.views.desc(), Recipe.cooking_time)
     )
-    recipes = result.scalars().all()
-    return recipes
+    return result.scalars().all()
 
 
 @app.get(
@@ -49,9 +51,7 @@ async def get_all_recipes(db: AsyncSession = Depends(get_db)) -> Sequence[Recipe
     summary="Get recipe by ID",
     description="Returns detailed information about a specific recipe.",
 )
-async def get_recipe_by_id(
-    recipe_id: int, db: AsyncSession = Depends(get_db)
-) -> Recipe:
+async def get_recipe_by_id(recipe_id: int, db: AsyncSession = get_db_dep) -> Recipe:
     result = await db.execute(select(Recipe).filter(Recipe.id == recipe_id))
     recipe = result.scalars().one_or_none()
 
@@ -66,9 +66,8 @@ async def get_recipe_by_id(
 
     # Повторно загружаем обновленный рецепт
     result = await db.execute(select(Recipe).filter(Recipe.id == recipe_id))
-    updated_recipe = result.scalars().one()
 
-    return updated_recipe
+    return result.scalars().one()
 
 
 @app.post(
@@ -77,7 +76,7 @@ async def get_recipe_by_id(
     summary="Add a new recipe",
     description="Creates a new recipe in the database.",
 )
-async def add_recipe(recipe: RecipeIn, db: AsyncSession = Depends(get_db)) -> Recipe:
+async def add_recipe(recipe: RecipeIn, db: AsyncSession = get_db_dep) -> Recipe:
     new_recipe = Recipe(**recipe.model_dump())
     db.add(new_recipe)
     await db.commit()
